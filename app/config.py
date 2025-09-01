8a46c1e496369df45fd35016934520d1462283e8
degregor69 <raphael.degregorio@gmail.com>
2025-09-01 09:19:29 +0200

    fixed db creation

--- app/config.py
+++ app/config.py
@@ -1,10 +1,15 @@
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = f"postgresql:{os.environ.get("DB_USER")}//{os.environ.get("DB_PASSWORD")}@localhost:{os.environ.get("DB_PORT")}/{os.environ.get("DB_NAME")}"
+load_dotenv()
+
+DATABASE_URL = (
+    f"postgresql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}"
+    f"@localhost:{os.environ.get('DB_PORT')}/{os.environ.get('DB_NAME')}"
+)
 
 engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(bind=engine)
-
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
